72b9df0c39643fac26188d3d60a84cce8c476882
Rocky Yu <rocky@quadtalent.com>
2022-09-16 21:49:22 +0800

    fix bug.

--- lattice-model/src/main/java/org/hifforce/lattice/annotation/model/ReduceType.java
+++ lattice-model/src/main/java/org/hifforce/lattice/annotation/model/ReduceType.java
@@ -6,8 +6,8 @@ package org.hifforce.lattice.annotation.model;
  */
 public enum ReduceType {
 
-    NONE,//对优先级排序完全不感知
-    ALL, //虽然都需要执行，但还是会感知优先级排序
-    FIRST, //对优先级排序非常敏感
-    UNKNOWN //还没有梳理的，需要梳理
+    NONE,
+    ALL,
+    FIRST,
+    UNKNOWN
 }
