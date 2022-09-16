c0e0e011d7f62709ce492c290408bba55087a2fa
Rocky Yu <rocky@quadtalent.com>
2022-09-16 21:48:48 +0800

    fix bug.

--- lattice-model/src/main/java/org/hifforce/lattice/annotation/model/ExtensionAnnotation.java
+++ lattice-model/src/main/java/org/hifforce/lattice/annotation/model/ExtensionAnnotation.java
@@ -19,25 +19,9 @@ public class ExtensionAnnotation {
 
     @Getter
     @Setter
-    private String activityCode;
-
-    @Getter
-    @Setter
     private String desc;
 
     @Getter
     @Setter
-    private String helpUrl;
-
-    @Getter
-    @Setter
-    private String tips;
-
-    @Getter
-    @Setter
     private ReduceType reduceType;
-
-    @Getter
-    @Setter
-    private String[] dependsOn;
 }
