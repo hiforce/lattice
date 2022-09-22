06a9563738d2aa9bb0f185931303c8530cd4d2fb
rocky <rocky@quadtalent.com>
2022-09-22 16:17:08 +0800

    opt code

--- lattice-model/src/main/java/org/hifforce/lattice/annotation/Business.java
+++ lattice-model/src/main/java/org/hifforce/lattice/annotation/Business.java
@@ -1,8 +1,6 @@
 package org.hifforce.lattice.annotation;
 
 
-import org.hifforce.lattice.model.business.TemplateType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
