a329f540fdd294c887e71d455962d5b607a9cc8c
Rocky Yu <rocky@quadtalent.com>
2022-12-02 22:28:28 +0800

    fix bug

--- lattice-remote/lattice-remote-container/src/main/java/org/hiforce/lattice/remote/container/service/LatticeRemoteInvokerImpl.java
+++ lattice-remote/lattice-remote-container/src/main/java/org/hiforce/lattice/remote/container/service/LatticeRemoteInvokerImpl.java
@@ -1,6 +1,5 @@
 package org.hiforce.lattice.remote.container.service;
 
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.hiforce.lattice.model.ability.IBusinessExt;
@@ -11,8 +10,6 @@ import org.hiforce.lattice.utils.BusinessExtUtils;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.util.List;
 
 /**
  * @author Rocky Yu
@@ -31,27 +28,23 @@ public class LatticeRemoteInvokerImpl implements LatticeRemoteInvoker {
                 .filter(p -> p.getExtensionCodes().contains(extCode))
                 .findFirst().orElse(null);
         if (null == realizationSpec) {
-            log.info("[Lattice] The realization not found. bizCode={}, scenario={}, extCode={} ", bizCode, scenario, extCode);
+            log.warn("[Lattice] The realization not found. bizCode={}, scenario={}, extCode={} ", bizCode, scenario, extCode);
             return null;
         }
         try {
             IBusinessExt businessExt = realizationSpec.getBusinessExt().getBusinessExtByCode(extCode, scenario);
-            log.info("[Lattice] The BusinessExt={}, bizCode={}, scenario={}, extCode={}",
+            log.debug("[Lattice] The BusinessExt={}, bizCode={}, scenario={}, extCode={}",
                     businessExt, bizCode, scenario, extCode);
 
             Method method = BusinessExtUtils.getExtensionMethod(businessExt, extCode, scenario);
-            log.info("[Lattice] The BusinessExt Method={}, bizCode={}, scenario={}, extCode={}",
-                    method.getName(), bizCode, scenario, extCode);
-
             Serializable value = (Serializable) method.invoke(businessExt, params);
-            log.info("[Lattice] Remote invoke bizCode={}, scenario={}, extCode={}, result={}",
+            log.debug("[Lattice] Remote invoke bizCode={}, scenario={}, extCode={}, result={}",
                     bizCode, scenario, extCode,
                     null == value ? null : value.toString());
             return value;
         } catch (Exception e) {
             log.info("[Lattice] Remote invoke runtime exception occurred. ex=bizCode={}, scenario={}, extCode={}, ex={}",
                     bizCode, scenario, extCode, e.getMessage());
-            log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
