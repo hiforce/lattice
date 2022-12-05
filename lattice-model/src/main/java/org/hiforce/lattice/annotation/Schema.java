8223a60acf8b184b85242b8672c7b063bc04f9a4
Rocky Yu <rocky@quadtalent.com>
2022-12-05 18:58:38 +0800

    add schema annotation for default business facade

---
+++ lattice-model/src/main/java/org/hiforce/lattice/annotation/Schema.java
@@ -0,0 +1,22 @@
+package org.hiforce.lattice.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Rocky Yu
+ * @since 2022/9/15
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Schema {
+
+    /**
+     * the schema's name.
+     */
+    String name();
+
+    /**
+     * whether the schema is root
+     */
+    boolean root() default false;
+}
